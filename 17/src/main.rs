72a47979f97d791593fc63ba2ea11ac01c2a41e8
beulard <beulard@gmail.com>
2024-01-06 01:55:34 +0100

    17 done

--- 17/src/main.rs
+++ 17/src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     cmp::Reverse,
     collections::{BinaryHeap, HashMap, HashSet, VecDeque},
-    fmt::Binary,
 };
 
 #[derive(Debug)]
@@ -32,25 +31,6 @@ impl Map {
     fn get(&self, position: Position) -> u8 {
         self.data[position.0 + position.1 * self.width]
     }
-}
-
-fn main() {
-    let input = std::fs::read_to_string("input.txt").unwrap();
-    let map = Map::new(&input);
-
-    // println!("{:?}", Dot::new(&graph));
-
-    let (cost, path) = dijkstra(
-        &map,
-        (0, 0),
-        (map.width - 1, map.height - 1),
-        // (4, 0),
-    );
-    // dbg!(&path, cost);
-
-
-    draw_path(&map, &path);
-    println!("Minimized cost: {}", cost);
 }
 
 fn draw_path(map: &Map, path: &Vec<State>) {
@@ -96,82 +76,6 @@ struct State {
     consecutive: usize,
 }
 
-fn dijkstra(map: &Map, start: Position, end: Position) -> (u32, Vec<State>) {
-    let mut score: HashMap<State, u32> = HashMap::new();
-    let mut prev: HashMap<State, State> = HashMap::new();
-    let mut pq: BinaryHeap<MinScored> = BinaryHeap::new();
-
-    score.insert(State {
-        pos: start,
-        direction: Direction::Up,
-        consecutive: 1,
-    }, 0);
-
-    pq.push(MinScored(0, State {
-        pos: start,
-        direction: Direction::Up,
-        consecutive: 0,
-    }));
-
-    while let Some(MinScored(dist, node)) = pq.pop() {
-
-        if node.pos == end {
-            let mut cur = node;
-            let mut path = vec![cur];
-            while let Some(p) = prev.get(&cur) {
-                path.push(*p);
-                cur = *p;
-            }
-            path.reverse();
-
-            return (dist, path);
-        }
-        let neighbors = get_neighbors(node.pos, map, vec![]);
-
-        for tgt in neighbors {
-            let dir = get_direction(&node.pos, &tgt);
-            // Cannot go backward
-            if is_opposite(dir, node.direction) {
-                continue;
-            }
-            let consec = if dir == node.direction {
-                node.consecutive + 1
-            } else {
-                1
-            };
-
-            // Cannot go more than 3 steps in the same direction
-            if consec > 3 {
-                continue;
-            }
-
-            // dbg!(node);
-            // dbg!(tgt);
-            // dbg!(dir, consec);
-            let state = State {
-                pos: tgt,
-                consecutive: consec,
-                direction: dir,
-            };
-            let alt = dist + map.get(tgt) as u32;
-            if let Some(s) = score.get(&state) {
-                if alt < *s {
-                    score.insert(state, alt);
-                    prev.insert(state, node);
-                    pq.push(MinScored(alt, state));
-                }
-            } else {
-                score.insert(state, alt);
-                prev.insert(state, node);
-                pq.push(MinScored(alt, state));
-            }
-            // dbg!(&score);
-        }
-    }
-
-    unreachable!();
-}
-
 fn get_neighbors(pos: Position, map: &Map, forbidden_directions: Vec<Direction>) -> Vec<Position> {
     let mut neighbors = vec![];
     use Direction::*;
@@ -212,25 +116,216 @@ fn get_direction(src: &Position, tgt: &Position) -> Direction {
     }
 }
 
+/// MinScoredState: implementation of priority queue with a BinaryHeap of States
+/// Because BinaryHeap orders elements in ascending order, we specify Ord for MinScoredState.
 #[derive(Debug)]
-struct MinScored(u32, State);
+struct MinScoredState(u32, State);
 
-impl PartialEq for MinScored {
+impl PartialEq for MinScoredState {
     fn eq(&self, other: &Self) -> bool {
         self.0 == other.0
     }
 }
 
-impl Eq for MinScored {}
+impl Eq for MinScoredState {}
 
-impl PartialOrd for MinScored {
+impl PartialOrd for MinScoredState {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Reverse(self.0).partial_cmp(&Reverse(other.0))
     }
 }
 
-impl Ord for MinScored {
+impl Ord for MinScoredState {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         Reverse(self.0).cmp(&Reverse(other.0))
     }
+}
+
+/// Dijkstra shortest path for crucibles (at most 3 steps in the same direction, cannot go backward).
+/// Uses "stateful" steps in the 2D grid, with each having a direction and remembering
+/// the number of consecutive steps in that direction.
+fn dijkstra(map: &Map, start: Position, end: Position) -> (u32, Vec<State>) {
+    let mut score: HashMap<State, u32> = HashMap::new();
+    let mut prev: HashMap<State, State> = HashMap::new();
+    let mut pq: BinaryHeap<MinScoredState> = BinaryHeap::new();
+
+    score.insert(
+        State {
+            pos: start,
+            direction: Direction::Up,
+            consecutive: 1,
+        },
+        0,
+    );
+
+    pq.push(MinScoredState(
+        0,
+        State {
+            pos: start,
+            direction: Direction::Up,
+            consecutive: 0,
+        },
+    ));
+
+    while let Some(MinScoredState(dist, node)) = pq.pop() {
+        if node.pos == end {
+            let mut cur = node;
+            let mut path = vec![cur];
+            while let Some(p) = prev.get(&cur) {
+                path.push(*p);
+                cur = *p;
+            }
+            path.reverse();
+
+            return (dist, path);
+        }
+        let neighbors = get_neighbors(node.pos, map, vec![]);
+
+        for tgt in neighbors {
+            let dir = get_direction(&node.pos, &tgt);
+            let mut consec = 1;
+            // Do not apply to first step
+            if node.pos != (0, 0) {
+                // Cannot go backward
+                if is_opposite(dir, node.direction) {
+                    continue;
+                }
+                consec = if dir == node.direction {
+                    node.consecutive + 1
+                } else {
+                    1
+                };
+
+                // Cannot go more than 3 steps in the same direction
+                if consec > 3 {
+                    continue;
+                }
+            }
+
+            let state = State {
+                pos: tgt,
+                consecutive: consec,
+                direction: dir,
+            };
+            let alt = dist + map.get(tgt) as u32;
+
+            // Compare new distance with previously stored one
+            // If the previous value was larger than the new one, or if no value was stored,
+            // update the queue and path.
+            if alt < *score.get(&state).unwrap_or(&u32::MAX) {
+                score.insert(state, alt);
+                prev.insert(state, node);
+                pq.push(MinScoredState(alt, state));
+            }
+        }
+    }
+    unreachable!();
+}
+
+/// Dijkstra shortest path for ultra crucibles
+fn ultra_dijkstra(map: &Map, start: Position, end: Position) -> (u32, Vec<State>) {
+    let mut score: HashMap<State, u32> = HashMap::new();
+    let mut prev: HashMap<State, State> = HashMap::new();
+    let mut pq: BinaryHeap<MinScoredState> = BinaryHeap::new();
+
+    score.insert(
+        State {
+            pos: start,
+            direction: Direction::Up,
+            consecutive: 1,
+        },
+        0,
+    );
+
+    pq.push(MinScoredState(
+        0,
+        State {
+            pos: start,
+            direction: Direction::Up,
+            consecutive: 0,
+        },
+    ));
+
+    while let Some(MinScoredState(dist, node)) = pq.pop() {
+        // End condition must take into account minimum
+        // consecutive steps for ultra crucible
+        if node.pos == end && node.consecutive > 3 {
+            let mut cur = node;
+            let mut path = vec![cur];
+            while let Some(p) = prev.get(&cur) {
+                path.push(*p);
+                cur = *p;
+            }
+            path.reverse();
+
+            return (dist, path);
+        }
+        let neighbors = get_neighbors(node.pos, map, vec![]);
+
+        for tgt in neighbors {
+            let dir = get_direction(&node.pos, &tgt);
+            let mut consec = 1;
+            // Do not apply to first step
+            if node.pos != (0, 0) {
+                // Cannot go backward
+                if is_opposite(dir, node.direction) {
+                    continue;
+                }
+                // Have to keep going straight if consec < 4
+                if node.consecutive < 4 && dir != node.direction {
+                    continue;
+                }
+                consec = if dir == node.direction {
+                    node.consecutive + 1
+                } else {
+                    1
+                };
+
+                // Cannot go in this direction if > 10 steps in the same direction
+                if consec > 10 {
+                    continue;
+                }
+            }
+
+            let state = State {
+                pos: tgt,
+                consecutive: consec,
+                direction: dir,
+            };
+            let alt = dist + map.get(tgt) as u32;
+
+            // Compare new distance with previously stored one
+            // If the previous value was larger than the new one, or if no value was stored,
+            // update the queue and path.
+            if alt < *score.get(&state).unwrap_or(&u32::MAX) {
+                score.insert(state, alt);
+                prev.insert(state, node);
+                pq.push(MinScoredState(alt, state));
+            }
+        }
+    }
+    unreachable!();
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let map = Map::new(&input);
+
+    let (cost, path) = dijkstra(
+        &map,
+        (0, 0),
+        (map.width - 1, map.height - 1),
+        // (4, 0),
+    );
+    // dbg!(&path, cost);
+
+    draw_path(&map, &path);
+    println!("Minimized cost: {}", cost);
+
+    // Part 2
+
+    let (cost, path) = ultra_dijkstra(&map, (0, 0), (map.width - 1, map.height - 1));
+
+    draw_path(&map, &path);
+    println!("Minimized cost for ultra crucible: {}", cost);
 }
