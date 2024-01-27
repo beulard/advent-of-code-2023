d0acf06dec5441634dfcb8b2ba7359555cc42e2d
Matthias Dubouchet <beulard@gmail.com>
2024-01-28 00:36:46 +0100

    wip

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -49,14 +49,61 @@ fn get_reachable_tiles(n_steps: usize, current_pos: HashSet<Position>, map: &Has
     get_reachable_tiles(n_steps-1, reachable, map)
 }
 
-fn main() {
+fn part1() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     let (start, tiles) = parse_input(&input);
     // dbg!(&start, &tiles);
 
-    let r = get_reachable_tiles(64, HashSet::from([start]), &tiles);
+    let r = get_reachable_tiles(50, HashSet::from([start]), &tiles);
     // dbg!(&r);
 
     dbg!(r.len());
+}
+
+fn get_reachable_tiles_p2(n_steps: usize, current_pos: HashSet<Position>, map: &HashMap<Position, Tile>, width: usize, height: usize) -> HashSet<Position> {
+    if n_steps == 0 {
+        return current_pos;
+    }
+
+    let mut reachable = HashSet::new();
+
+
+    for pos in current_pos {
+        // Try up
+        for delta in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+            let next = ((pos.0 + delta.0).rem_euclid(width as isize), (pos.1 + delta.1).rem_euclid(height as isize));
+            // dbg!((pos.0 + delta.0, pos.1 + delta.1));
+            // dbg!(next);
+            if let Some(tile) = map.get(&next) {
+                if *tile == Tile::Garden {
+                    reachable.insert((pos.0 + delta.0, pos.1 + delta.1));
+                }
+            }
+        }
+    }
+
+    get_reachable_tiles_p2(n_steps-1, reachable, map, width, height)
+}
+
+fn part2() {
+    
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let height = input.lines().count();
+    let width = input.lines().nth(0).unwrap().len();
+
+    let (start, tiles) = parse_input(&input);
+    // dbg!(&start, &tiles);
+
+    let r = get_reachable_tiles_p2(50, HashSet::from([start]), &tiles, width, height);
+    // dbg!(&r);
+
+    dbg!(r.len());
+
+    dbg!((-19_i32).rem_euclid(10));
+}
+
+fn main() {
+    part1();
+    part2();
 }
